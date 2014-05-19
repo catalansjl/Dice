97cbced44ec0fbaeac7ff7505b9b6c95c5b0e985
Jorge Catalan <jesus@192.168.1.123>
2014-05-18 21:17:23 -0700

    a

---
+++ Dice/src/DiceTest.java
@@ -0,0 +1,25 @@
+
+public class DiceTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub		
+						
+		 Dice myRandomNumber = new  Dice();         
+				
+		
+		myRandomNumber.Throw(); 	
+		
+		 int jorge=myRandomNumber.Value(); 	
+
+		 System.out.println(+ jorge);
+ 
+   }	 
+}
+
+
+
+		
+		
+		
+
+	
